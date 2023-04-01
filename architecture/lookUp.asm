b4ebad46b3d11cbe6da5b8378870e750e1fb8e77
ys <you@example.com>
2023-04-02 01:51:09 +0800

    lab1 : x86

---
+++ architecture/lookUp.asm
@@ -0,0 +1,119 @@
+DATA SEGMENT
+    BUFFER DB 99
+    LEN    DB ?                                    ; record length of string
+    STRING DB 99 DUP(0)                            ; real sentence
+    INFO   DB "name: Yusen Wu, id: 2000013137$"
+    POSI   DB "found$"
+    NEGA   DB "not found$"
+DATA ENDS
+
+STACK SEGMENT STACK
+    STA   DB  50 DUP(?)
+    TOP   EQU LENGTH STA
+STACK ENDS
+
+CODE SEGMENT
+             ASSUME CS:CODE, DS:DATA, SS:STACK
+    BEGIN:   
+    ; load stack data in
+             MOV    AX, DATA
+             MOV    DS, AX
+             MOV    AX, STACK
+             MOV    SS, AX
+             MOV    AX, TOP
+             MOV    SP, AX
+             MOV    AX, DATA
+             MOV    ES, AX
+     
+             MOV    DX, OFFSET BUFFER             ; read in buffer string
+             MOV    AH, 0AH
+             INT    21H
+    ; output enter
+             MOV    DL, 0DH
+             MOV    AH, 2
+             INT    21H
+    ; another line
+             MOV    DL, 0AH
+             MOV    AH, 2
+             INT    21H
+    CYCLE:   
+             MOV    AH, 07H                       ; read one character in and cycle
+             INT    21H
+             CMP    AL, 1BH                       ; current input: esc
+             JNE    SETTLE
+             JMP    NEAR PTR EXIT
+    SETTLE:                                       ; search for given character with bunch method
+             MOV    DI, OFFSET STRING             ; original sentence
+             MOV    BX, OFFSET BUFFER             ; target sentences
+             ADD    BX, 1H                        ; get sentence length address
+             CLD
+             MOV    CL, BYTE PTR[BX]              ; set length to CL
+             MOV    CH, 0
+    SEARCH:                                       ; bunch operation search function
+             REPNE  SCASB                         ; kernel search function
+             JNZ    NOTFOUND                      ; if find one fitted just jump to success procedure
+    FOUND:   
+    ; output suc string
+             MOV    DX, OFFSET POSI
+             MOV    AH, 09H
+             INT    21H
+    ; calculate character searched's index
+             MOV    DL, LEN
+             SUB    DX, CX                        ; sub length: get relative index
+    ;          ADD    DX, 30H                       ; add ascii number 0 to relative: get absolute numnber of index
+    ; TODO: if length too large may get ;<> instead of 10,11,12... so change output method
+    ;          MOV    AH, 2H
+    ;          INT    21H
+
+    ; currently number is in DX
+             MOV    AX, DX                        ; move from dx to store in ax
+             MOV    BL, 10
+             DIV    BL                            ;calculate AX/BL, right on AH，result on AL
+             MOV    DX, AX
+             CMP    DL, 0
+             JE     SINGLE
+             ADD    DL, 30H                       ;transform into 10-ascii code
+             MOV    AH, 2H
+             INT    21H
+    SINGLE:  
+             MOV    DL, DH
+             ADD    DL, 30H
+             MOV    AH, 2H
+             INT    21H
+
+
+    ; enter
+             MOV    DL, 0DH
+             MOV    AH, 2
+             INT    21H
+    ; another line
+             MOV    DL, 0AH
+             MOV    AH, 2
+             INT    21H
+             JMP    NEAR PTR CYCLE
+    NOTFOUND:
+    ; output fai string
+             MOV    DX, OFFSET NEGA
+             MOV    AH, 09H
+             INT    21H
+    ; output enter and change line directly without changing into ascii number or so
+    ; enter
+             MOV    DL, 0DH
+             MOV    AH, 2
+             INT    21H
+    ; another line
+             MOV    DL, 0AH
+             MOV    AH, 2
+             INT    21H
+             JMP    NEAR PTR CYCLE
+    EXIT:    
+    ; output personal infomation
+             MOV    DX, OFFSET INFO
+             MOV    AH, 09H
+             INT    21H
+    ; return
+             MOV    AX, 4C00H
+             INT    21H
+
+CODE ENDS
+    END BEGIN
--- architecture/sentence.asm
+++ architecture/sentence.asm
@@ -1,84 +1,84 @@
-DATA SEGMENT							;数据段
-	BUFFER		DB	81					;用于接受从键盘输入的字符串的BUFFER数组，最长可接受81个字符
-				DB	0					;用于储存实际输入字符串的长度
-	STRING		DB	81	DUP(0)			;实际接收到的字符串
-	NAME_AND_ID	DB	'NAME:Pei Mingyuan; ID:1200012896.$'	;作者的姓名和学号
-	SUC			DB	'Success!!! Location: $'				;查找成功时输出的信息
-	FAI			DB	'Fail!!!$'								;查找失败时输出的信息
+DATA SEGMENT                                  		;数据段
+	BUFFER      DB 81                             	;用于接受从键盘输入的字符串的BUFFER数组，最长可接受81个字符
+	            DB 0                              	;用于储存实际输入字符串的长度
+	STRING      DB 81	DUP(0)                      	;实际接收到的字符串
+	NAME_AND_ID DB 'NAME:Yusen Wu; ID:2000013137$'	;作者的姓名和学号
+	SUC         DB 'found$'                       	;查找成功时输出的信息
+	FAI         DB 'not found$'                   	;查找失败时输出的信息
 DATA ENDS
-STACK SEGMENT STACK						;堆栈段
-    STA     DB 50 DUP(?)
-    TOP     EQU LENGTH STA
+STACK SEGMENT STACK 		;堆栈段
+	STA   DB  50 DUP(?)
+	TOP   EQU LENGTH STA
 STACK ENDS
-CODE SEGMENT							;数据段
-    ASSUME CS:CODE, DS:DATA, SS:STACK
-    BEGIN:  MOV AX, DATA
-            MOV DS, AX			;为DS赋初值
-			MOV AX, DATA
-            MOV ES, AX			;为ES赋初值
-            MOV AX, STACK
-            MOV SS, AX			;为SS赋初值
-            MOV AX, TOP
-            MOV SP, AX			;为SP赋初值
+CODE SEGMENT                              		;数据段
+	         ASSUME CS:CODE, DS:DATA, SS:STACK
+	BEGIN:   MOV    AX, DATA
+	         MOV    DS, AX                    	;为DS赋初值
+	         MOV    AX, DATA
+	         MOV    ES, AX                    	;为ES赋初值
+	         MOV    AX, STACK
+	         MOV    SS, AX                    	;为SS赋初值
+	         MOV    AX, TOP
+	         MOV    SP, AX                    	;为SP赋初值
 			
-			MOV	DX, OFFSET BUFFER	;从键盘缓冲区接受字符串
-			MOV	AH, 0AH
-			INT	21H
+	         MOV    DX, OFFSET BUFFER         	;从键盘缓冲区接受字符串
+	         MOV    AH, 0AH
+	         INT    21H
 			
-			MOV DL, 0DH			;输出回车
-			MOV AH, 2
-			INT 21H
-			MOV DL, 0AH			;输出换行
-			MOV AH, 2
-			INT 21H
+	         MOV    DL, 0DH                   	;输出回车
+	         MOV    AH, 2
+	         INT    21H
+	         MOV    DL, 0AH                   	;输出换行
+	         MOV    AH, 2
+	         INT    21H
 			
-	ROTATE:	MOV AH, 07H			;轮询开始的地方
-			INT 21H
-			CMP AL, 1BH			;若字符为esc，则退出
-			JNE	HANDLE			;否则进入处理流程
-			JMP	NEAR PTR STOP
-	HANDLE:	MOV	DI, OFFSET STRING	;设置好源操作串
-			MOV	BX, OFFSET BUFFER
-			ADD	BX, 1H				;得到记录串长度的地址
-			CLD
-			MOV	CL,	BYTE PTR[BX];	;将串长度付给CX
-			MOV CH, 0
-	NEXT:	REPNE SCASB				;使用串操作指令进行寻找
-			JZ	FOUND				;若找到，则进入FOUND流程
-NOTFOUND:	MOV	DX,	OFFSET FAI		;否则进入NOTFOUND流程
-			MOV AH,	09H				;输出FAI串
-			INT	21H
+	ROTATE:  MOV    AH, 07H                   	;轮询开始的地方
+	         INT    21H
+	         CMP    AL, 1BH                   	;若字符为esc，则退出
+	         JNE    HANDLE                    	;否则进入处理流程
+	         JMP    NEAR PTR STOP
+	HANDLE:  MOV    DI, OFFSET STRING         	;设置好源操作串
+	         MOV    BX, OFFSET BUFFER
+	         ADD    BX, 1H                    	;得到记录串长度的地址
+	         CLD
+	         MOV    CL,	BYTE PTR[BX]          	;	;将串长度付给CX
+	         MOV    CH, 0
+	NEXT:    REPNE  SCASB                     	;使用串操作指令进行寻找
+	         JZ     FOUND                     	;若找到，则进入FOUND流程
+	NOTFOUND:MOV    DX,	OFFSET FAI            	;否则进入NOTFOUND流程
+	         MOV    AH,	09H                   	;输出FAI串
+	         INT    21H
 			
-			MOV DL, 0DH			;输出回车
-			MOV AH, 2
-			INT 21H
-			MOV DL, 0AH			;输出换行
-			MOV AH, 2
-			INT 21H
+	         MOV    DL, 0DH                   	;输出回车
+	         MOV    AH, 2
+	         INT    21H
+	         MOV    DL, 0AH                   	;输出换行
+	         MOV    AH, 2
+	         INT    21H
 			
-			JMP	NEAR PTR ROTATE		;继续查找下一个字符
-	FOUND:	MOV	DX,	OFFSET SUC		;输出SUC串
-			MOV AH,	09H
-			INT	21H
-			MOV	DX,	[BX]			;根据现存的CX的值与串的长度，计算出该字符的位置
-			SUB	DX,	CX
-			ADD	DX,	30H				;将该位置转化为ASCII码，并输出
-			MOV	AH,	2H
-			INT 21H
+	         JMP    NEAR PTR ROTATE           	;继续查找下一个字符
+	FOUND:   MOV    DX,	OFFSET SUC            	;输出SUC串
+	         MOV    AH,	09H
+	         INT    21H
+	         MOV    DX,	[BX]                  	;根据现存的CX的值与串的长度，计算出该字符的位置
+	         SUB    DX,	CX
+	         ADD    DX,	30H                   	;将该位置转化为ASCII码，并输出
+	         MOV    AH,	2H
+	         INT    21H
 			
-			MOV DL, 0DH			;输出回车
-			MOV AH, 2
-			INT 21H
-			MOV DL, 0AH			;输出换行
-			MOV AH, 2
-			INT 21H
+	         MOV    DL, 0DH                   	;输出回车
+	         MOV    AH, 2
+	         INT    21H
+	         MOV    DL, 0AH                   	;输出换行
+	         MOV    AH, 2
+	         INT    21H
 			
-			JMP	NEAR PTR ROTATE		;继续查找下一个字符
-	STOP:	MOV	DX, OFFSET NAME_AND_ID	;程序末尾输出我的姓名和学号
-			MOV	AH, 09H
-			INT 21H
+	         JMP    NEAR PTR ROTATE           	;继续查找下一个字符
+	STOP:    MOV    DX, OFFSET NAME_AND_ID    	;程序末尾输出我的姓名和学号
+	         MOV    AH, 09H
+	         INT    21H
 			
-			MOV AX, 4C00H		;带返回码结束，AL=返回码
-            INT 21H
+	         MOV    AX, 4C00H                 	;带返回码结束，AL=返回码
+	         INT    21H
 CODE ENDS
             END BEGIN			;程序结束
